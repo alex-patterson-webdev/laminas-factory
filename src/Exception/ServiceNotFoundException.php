586aef9749d1e15e3ebf6e7de85af860c0860cb2
Alex Patterson <alex.patterson.webdev@gmail.com>
2020-11-15 13:26:39 +0000

    Added @deprecated annotation to ServiceNotCreatedException and ServiceNotFoundException

--- src/Exception/ServiceNotFoundException.php
+++ src/Exception/ServiceNotFoundException.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Arp\LaminasFactory\Exception;
 
 /**
+ * @deprecated Please use the Laminas Exception classes instead
+ *
  * @author  Alex Patterson <alex.patterson.webdev@gmail.com>
  * @package Arp\LaminasFactory\Exception
  */
